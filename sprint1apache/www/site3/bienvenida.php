5baaa0fe263ce02febf3b3abf932d528d1984259
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-10-16 12:24:24 +0200

    #Tarea 2 crear archivo.php que dá la bienvenida

---
+++ sprint1apache/www/site3/bienvenida.php
@@ -0,0 +1,19 @@
+                                  
+<html>
+        <head>
+                <title>Sprint 1 , parte 2,ejercicio 2</title>
+        </head>
+        <body>
+                <h1>Sprint 1,parte 2,ejercicio 2</h1>
+                <h2>Página de Bienvenidas</h2>
+		<?php
+			function dar_bienvenida ($nombre){
+				echo "!Bienvenido/a ".$nombre . "!";
+			}
+			dar_bienvenida("Homer");
+//			 echo "!Bienvenido/a Homer !";			
+		 ?>
+        </body>
+</html>
+
+ 
