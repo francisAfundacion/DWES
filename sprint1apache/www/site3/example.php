bb277e7d63507789bf008841db3bfe55fa7397b2
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-10-19 15:59:59 +0200

    Cambiar valores encabezados y agregar mensaje demi superheroe favorito en example.php

--- sprint1apache/www/site3/example.php
+++ sprint1apache/www/site3/example.php
@@ -4,8 +4,14 @@
                 <title>Sprint 1 parte 2</title>
         </head>
         <body>
-                <h1>Sprint 1 parte 2</h1>
-                <h2>Joker.php</h2>
+                <h1>Sprint 1 parte 2 Ejericio1</h1>
+                <h2>example.php</h2>
+		<h3>
+			<?php
+				$heroe = "FranciscoMan";
+				echo $heroe;
+			?>
+		</h3>
         </body>
 </html>
 
