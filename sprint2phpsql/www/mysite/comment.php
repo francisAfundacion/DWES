5c60d7e559fd7bbdea3c8075a6b6bfec9c7e37fd
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-10-26 18:59:55 +0200

    Modificar comment.php para insertar a la BD el comentario la fecha en la que se efectúo

--- sprint2phpsql/www/mysite/comment.php
+++ sprint2phpsql/www/mysite/comment.php
@@ -4,10 +4,12 @@
 	?>
     <body>
         <?php
+            $fecha = new DateTime();
+            $fecha_formateada =  $fecha->format('Y-m-d H-i-s');
             $id_libro = $_POST['id'];
             $nuevo_comentario = $_POST['new_comment'];
-            $consulta_insercion = "INSERT INTO tComentarios(libro_id,comentario,usuario_id) VALUES(".$id_libro.",
-            '".$nuevo_comentario."',1)";
+            $consulta_insercion = "INSERT INTO tComentarios(libro_id,comentario,usuario_id,fecha) VALUES(".$id_libro.",
+            '".$nuevo_comentario."',1,'".$fecha_formateada."')";
             mysqli_query($db,$consulta_insercion) or die ("Error!");
             echo "<p>Nuevo comentario</p>";
             echo mysqli_insert_id($db);
