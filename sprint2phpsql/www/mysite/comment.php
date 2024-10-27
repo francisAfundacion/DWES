0b521a004fdb83db1cacc5ae38260fd5a9b87ad7
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-10-27 12:57:38 +0100

    agregar css  interno comment.php

--- sprint2phpsql/www/mysite/comment.php
+++ sprint2phpsql/www/mysite/comment.php
@@ -1,8 +1,36 @@
 <html>
+    <style>
+        body{
+            background-color:hwb(69 87% 5%);
+            font-size:30px;
+            font-weight:bold;
+            text-shadow:1px 1px 1px black;
+            color:brown;
+            text-align:center;
+            letter-spacing:3px;
+        }
+        a{
+            margin:30px 0px;
+        }
+        a:link{
+            color:hsl(330, 80%, 27%);
+        }
+        a:visited{
+            color:brown;
+        }
+        a:active {
+            color:rgb(89, 72, 117);
+
+        }
+        div{
+            margin-top:8%;
+        }
+    </style>
 <?php
 		$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 	?>
     <body>
+        <div>
         <?php
             $fecha = new DateTime();
             $fecha_formateada =  $fecha->format('Y-m-d H-i-s');
@@ -17,5 +45,6 @@
             mysqli_close($db);
             echo "<a href=/detail.php?id='".$id_libro."'>Volver</a>";
         ?>
+        </div>
     
 </html>
