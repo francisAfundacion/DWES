52743a78dbe02f0a1f5a4d9141ee3a88f5612114
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-11-01 14:02:24 +0100

    Agregar login.php,posibilita iniciar sesión al usuario, pudiendo accedder a main.php en consecuencia.

---
+++ sprint3sesiones/www/mysite/login.php
@@ -0,0 +1,58 @@
+<?php
+    $db = mysqli_connect('localhost','root','1234','mysitedb');
+    /*
+    3. Añade un formulario login.html como el del apartado 3. Añade una página
+login.php que permita al usuario loguearse y redirija a la página principal. Verifica
+que funciona. Verifica el comportamiento si introduces un email que no existe o una
+contraseña incorrecta. Para comprobar la contraseña usa
+password_verify($pasword_introducido, $password_hasheado). Haz commit y
+push.
+    */
+    function comprobar_cuenta_existe ($email,$pass){
+        global $db;
+        $mensajetxt = "";
+        $consulta = "SELECT * FROM tUsuarios WHERE email='". $email ."'";
+        $resultado_consulta = mysqli_query($db,$consulta);
+        echo $fila['contraseña'];
+        if (mysqli_num_rows($resultado_consulta) == 0){
+            $mensajetxt = "¡ERROR!El email introducido no existe!";
+        }
+        else{ //existe el email
+            $fila = mysqli_fetch_array($resultado_consulta);
+            if(!password_verify($pass,$fila['contraseña'])){
+                $mensajetxt = "¡ERROR!La contraseña introducida es incorrecta.";
+            }
+            else{
+                session_start();
+                $_SESSION['id_usuario'] = $fila['id'];
+                echo $_SESSION['id_usuario'];
+                //header('location:main.php');
+            }
+        }
+        return $mensajetxt;
+    }
+?>
+<html>
+<head>
+    <title>Ejercicio 3</title>
+</head>
+<body>
+    <?php
+         if(!isset($_POST['email'])){
+            echo "No se ha enviado el email.";
+        }
+        if(!isset($_POST['fpass'])){
+            echo "no se ha enviado la contraseña.";
+        }
+        $email = $_POST['email'];
+        $pass = $_POST['fpass'];
+        if (!empty($email) and !empty($pass)){
+            echo comprobar_cuenta_existe($email,$pass);
+              
+        }
+        else{
+            echo "El correo electrónico y/o la contraseña se han  enviado vacíos.";
+        }
+    ?>
+</body>
+</html>
