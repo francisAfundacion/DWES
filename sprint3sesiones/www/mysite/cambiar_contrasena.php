de30ec9d09e8f9e790e038a524ea7b08d1f89b3f
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-11-07 20:31:27 +0100

    quitar parámetro inncesesario  de la función cambiar_contrasena_bd y añadir documentación en cambiar_contrasena.php

--- sprint3sesiones/www/mysite/cambiar_contrasena.php
+++ sprint3sesiones/www/mysite/cambiar_contrasena.php
@@ -4,11 +4,24 @@
     </head>
     <body>
         <?php
+            // Establece la conexión a la base de datos 'mysitedb' usando los parámetros proporcionados
             $db = mysqli_connect('localhost','root','1234','mysitedb');
+             /**
+            * Función para comprobar las contraseñas del usuario.
+            *
+            * Esta función valida que la contraseña antigua coincida con la que está almacenada en la base de datos,
+            * que la nueva contraseña no sea igual a la antigua y que la nueva contraseña coincida con la confirmación.
+            *
+            * @param string $vieja Contraseña antigua proporcionada por el usuario
+            * @param string $nueva Nueva contraseña proporcionada por el usuario
+            * @param string $confirmacion Confirmación de la nueva contraseña
+            * @return string Mensaje de error si alguna validación falla, cadena vacía si todo es correcto
+            */
             function comprobar_contrasenas($vieja, $nueva,$confirmacion){
-                session_start();
+                session_start(); // abrir la sesión para acceder al id de usuario
                 global $db;
                 $mensajetxt = "";
+                // Consulta SQL para obtener la contraseña almacenada del usuario con el ID almacenado en la sesión
                 $consulta_contrasena_usuario = $db -> prepare("SELECT contraseña from tUsuarios where id=?");
                 $consulta_contrasena_usuario -> bind_param("i",$_SESSION['id_usuario']);
                 $consulta_contrasena_usuario -> execute();
@@ -30,11 +43,19 @@
                 }
                 return $mensajetxt;
             }
-            
-            function cambiar_contrasena_bd($nueva,$email){
+            /** 
+            * Función para actualizar la contraseña del usuario en la base de datos.
+            *
+            * Esta función recibe la nueva contraseña, la encripta y luego la actualiza
+            * en la base de datos para el usuario cuya sesión está activa.
+            *
+            * @param string $nueva Nueva contraseña que será asignada al usuario
+            */
+            function cambiar_contrasena_bd($nueva){
                 global $db;
-                session_start();
+                session_start(); // abrir la sesión para acceder al id de usuario
                 $nueva_contrasena_hasheada = password_hash($nueva,PASSWORD_DEFAULT);
+                // Consulta SQL para modificar la contraseña del id del usuario guardada en la sesión actual.
                 $consulta_modif_contrasena = $db -> prepare("UPDATE tUsuarios SET contraseña=? where id=?");
                 $consulta_modif_contrasena -> bind_param("si",$nueva_contrasena_hasheada,$_SESSION['id_usuario']);
                 $consulta_modif_contrasena -> execute();
@@ -46,14 +67,16 @@
         <?php
             $valido=true;
             $mensajetxt="";
+             //Comprueba que los valores del formulario han llegado correctamente
             if (!isset($_POST['flast_pass']) or !isset($_POST['fnew_pass']) or !isset($_POST['fnew_pass_confirm'])){
                 echo "<p>¡ERROR!Alguno de los campos para cambiar la contraseña no han llegado correctamente!</p>";
             }
             else{
+                //Asigna los valores del formulario a variables.
                 $last_pass = $_POST['flast_pass'];
                 $new_pass = $_POST['fnew_pass'];
                 $new_pass_confirm =  $_POST['fnew_pass_confirm'];
-
+                //Comprobar si los valores del formulario han llegado vacíos
                 if (empty($last_pass)) {
                     echo "<p>ERROR¡Se ha dejado vacío el campo relativo a la antigua contraseña!</p>";
                     $valido=false;
