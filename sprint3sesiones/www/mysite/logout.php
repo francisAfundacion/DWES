374f461fef6a78c6aa41fc8a8bd9e7c20a3cffbc
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-11-07 21:16:30 +0100

    Agregar documentación logout.php

--- sprint3sesiones/www/mysite/logout.php
+++ sprint3sesiones/www/mysite/logout.php
@@ -1,5 +1,9 @@
 <?php
     $db = mysqli_connect('localhost','root','1234','mysitedb');
+    /** 
+    *Función que permite cerrar sesión al usuario, tras haber elimiando el valor contenido de la sesión,
+    *redirigiendo al mismo  a la página para iniciar sesión.
+    */
     function cerrar_sesion(){
         global $db;
         session_start();
