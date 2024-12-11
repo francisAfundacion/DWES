65bd6e693be0b82a7d189b8a915110819855f1c2
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-12-11 21:37:20 +0100

    Eliminar redundancia vista extra index

--- django_tutorial/polls/views.py
+++ django_tutorial/polls/views.py
@@ -5,8 +5,6 @@ from django.urls import reverse
 
 from .models import Choice, Question
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
