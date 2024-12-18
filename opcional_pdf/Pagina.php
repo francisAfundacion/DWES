40a1250f0402984e203b3ad4b5f2b55d7076ce46
FranciscoGómez <fgomezs@fpcoruna.afundacion.org>
2024-12-18 17:29:46 +0100

    Reescribir método Header de la clase FPDF.
    Probar método Header al generar una página de la subclase PDF.
    
    Arreglar bug en la comprobación de si la query param es una cadena válida en validar_campo.

--- opcional_pdf/Pagina.php
+++ opcional_pdf/Pagina.php
@@ -1,15 +1,23 @@
 <?php
+    //habilitar mensajeserrores
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
     require('fpdf186/fpdf.php');
     class PDF extends FPDF{
-        function cabecera() {
-
+        function Header () {
+            $ancho_pag = $this -> GetPagewidth();
+            $titulo = "Certificado Desarrollo de Aplicaciones Web";
+            $this -> Image('img/logo.png',0,0, 50, 50);
+            $this -> SetFont('Times', 'B', 18);
+            $this -> ln(40);
+            $this -> Cell($ancho_pag,10, $titulo, 0, 1,'C');
         }
     }
     function validar_campo ($campo) {
         $LETRAS = "abcdefghijklmnopqrstuvwxyz";
         $campo = strtolower($campo);
         for ($pos_cadena = 0; $pos_cadena < strlen($campo) ; $pos_cadena ++){
-            if (!stripos($LETRAS, $campo[$pos_cadena])) {
+            if (stripos($LETRAS, $campo[$pos_cadena]) === false) {
                 die ("<p>El campo pasado no es una cadena válida, ya que contiene caracteres no permitidos.</p>");
             }
         }
@@ -20,7 +28,7 @@
             die ("<p>No ha  llegado correctamente el nombre del diplomado.</p>");
         }
         else {
-            echo $_GET['name'];
+            //echo $_GET['name'];
             validar_campo($_GET['name']);
         }
 
@@ -28,10 +36,13 @@
             die ("<p>No ha llegado correctamente el apellido del diplomado.</p>");
         }
         else {
-            echo $_GET['surname'];
+            //echo $_GET['surname'];
             validar_campo($_GET['surname']);
         }
         
     }
     comprobar_query_params();
+    $pdf  = new PDF();
+    $pdf -> AddPage();
+    $pdf -> Output();
 ?>
