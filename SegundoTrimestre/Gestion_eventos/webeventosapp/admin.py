4a89bd8df33959acbe7cf1412265e2e31a62cfc0
FranciscoGomez <fgomezs@fpcoruna.afundacion.org>
2025-01-18 18:01:32 +0100

    Semana_1: Personalizar visualización de la tabla eventos en la administración en el archivo admin.py.

--- SegundoTrimestre/Gestion_eventos/webeventosapp/admin.py
+++ SegundoTrimestre/Gestion_eventos/webeventosapp/admin.py
@@ -4,10 +4,16 @@ from .models import Evento
 from .models import Reserva
 from .models import Comentario
 
+class EventoAdmin (admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion', 'fecha', 'usuario')
+    search_fields = ('nombre', 'fecha')
+    list_filter = ('fecha' ,)
+
+admin.site.register(Evento,EventoAdmin)
 admin.site.register(UsuarioPersonalizado)
-admin.site.register(Evento)
 admin.site.register(Reserva)
 admin.site.register(Comentario)
+
 
 
 # Register your models here.
