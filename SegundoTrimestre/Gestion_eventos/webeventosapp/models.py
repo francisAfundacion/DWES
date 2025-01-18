3f647e8af9bff4b2d54bfdd632ca811686215e59
FranciscoGomez <fgomezs@fpcoruna.afundacion.org>
2025-01-18 16:32:45 +0100

    Semana_1: Agregar models.py con la creación de las clases para la aplicación webeventosapp.

--- SegundoTrimestre/Gestion_eventos/webeventosapp/models.py
+++ SegundoTrimestre/Gestion_eventos/webeventosapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
 # Create your models here.
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
+
+class UsuarioPersonalizado(AbstractUser):
+    TIPO_USUARIO = [
+        ('organizador', 'Organizador'),
+        ('participante', 'Participante')
+    ]
+    tipo = models.CharField(max_length = 20, choices = TIPO_USUARIO)
+    biografia = models.CharField(max_length= 100 ,null = True)
+
+class Evento(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.CharField(max_length=100)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    max_asistencias = models.IntegerField()
+    usuario = models.ForeignKey(UsuarioPersonalizado, on_delete = models.CASCADE)
+
+    def __str__ (self):
+        return self.nombre
+
+class Reserva(models.Model):
+    TIPO_ESTADO = [
+        ('pendiente', 'Pendiente'),
+        ('confirmada', 'Confirmada'),
+        ('cancelada', 'Cancelada')
+    ]
+    estado = models.CharField(max_length = 20 , choices = TIPO_ESTADO ,default = "pendiente")
+    entradas_reservadas = models.IntegerField()
+    usuario = models.ForeignKey( UsuarioPersonalizado, on_delete=models.CASCADE)
+    evento = models.ForeignKey(Evento, on_delete = models.CASCADE)
+
+class Comentario(models.Model):
+    texto = models.CharField(max_length = 100)
+    fecha = models.DateTimeField(default = datetime.now())
+    usuario = models.ForeignKey(UsuarioPersonalizado, on_delete = models.CASCADE)
+    evento = models.ForeignKey(Evento, on_delete = models.CASCADE)
+
+
+
