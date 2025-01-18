1ec41cd0a66073516e203a7c7eb94f4d97371988
FranciscoGomez <fgomezs@fpcoruna.afundacion.org>
2025-01-18 17:30:13 +0100

    Semana_1: Agregar importaciones de los modelos creados en admin.py
    Permitir gestión de los modelos a partir de la interfaz de administración que ofrece django

--- SegundoTrimestre/Gestion_eventos/webeventosapp/admin.py
+++ SegundoTrimestre/Gestion_eventos/webeventosapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import UsuarioPersonalizado
+from .models import Evento
+from .models import Reserva
+from .models import Comentario
+
+admin.site.register(UsuarioPersonalizado)
+admin.site.register(Evento)
+admin.site.register(Reserva)
+admin.site.register(Comentario)
+
 
 # Register your models here.
