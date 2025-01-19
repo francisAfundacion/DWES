ff749e4fb2d1c4b905e37b8e3054f58e1ad34848
FranciscoGomez <fgomezs@fpcoruna.afundacion.org>
2025-01-19 14:55:37 +0100

    Semana_1 : Agregar argumento blank en el campo biografía en la tabla UsuarioPersonalizado.

--- SegundoTrimestre/Gestion_eventos/webeventosapp/models.py
+++ SegundoTrimestre/Gestion_eventos/webeventosapp/models.py
@@ -10,7 +10,7 @@ class UsuarioPersonalizado(AbstractUser):
         ('participante', 'Participante')
     ]
     tipo = models.CharField(max_length = 20, choices = TIPO_USUARIO)
-    biografia = models.CharField(max_length = 300 ,null = True)
+    biografia = models.CharField(max_length = 300 ,null = True, blank = True)
 
 class Evento(models.Model):
     nombre = models.CharField(max_length = 100)
