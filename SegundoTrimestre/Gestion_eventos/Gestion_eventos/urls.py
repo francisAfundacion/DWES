92e8109c8e0e3841b739603e949fe1351a0de054
Francisco Gómez <fgomezs@fpcoruna.afundacion.org>
2025-02-03 18:14:16 +0100

    Semana 2: Actualizar

--- SegundoTrimestre/Gestion_eventos/Gestion_eventos/urls.py
+++ SegundoTrimestre/Gestion_eventos/Gestion_eventos/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from webeventosapp import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('listar_eventos', views.listar_eventos),
+    path('crear_evento', views.crear_evento),
     path('admin/', admin.site.urls),
+    path('actualizar_evento/<int:id>', views.actualizar_evento),
+    path('eliminar_evento/<int:id>', views.eliminar_evento),
+    path('listar_reservas', views.listar_reservas),
+    path('crear_reserva', views.crear_reserva),
+    path('actualizar_reserva/<int:id>', views.actualizar_reserva),
+    path('eliminar_reserva/<int:id>', views.eliminar_reserva),
+    path('listar_comentario/evento/<int:id>', views.listar_comentarios),
+    path('crear_comentario', views.crear_comentario),
+    path('login', views.login),
+    path('register', views.register),
 ]
